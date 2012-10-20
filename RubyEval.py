1af27e3eb33cd3510cdde6b7e70db8afaed15fda
jugyo <kkohno@gmail.com>
2012-10-21 01:57:02 +0900

    change to evaluate each lines independently
    and refactor

--- RubyEval.py
+++ RubyEval.py
@@ -1,47 +1,56 @@
-import subprocess, os
+import subprocess
 import sublime, sublime_plugin
 
 class RubyEvalCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        input_str = ""
         for region in self.view.sel():
-            input_str += self.view.substr(region) + "\n"
-
-        if input_str != "":
-            line_eval = False
-        else:
-            line_eval = True
-            for region in self.view.sel():
+            if region.a == region.b:
+                # eval line
                 region_of_line = self.view.line(region)
-                input_str += self.view.substr(region_of_line) + "\n"
+                script = self.view.substr(region_of_line)
+                output = self.eval_as_ruby(script)
+                self.view.insert(edit, region_of_line.b, "\n" + output)
+            else:
+                # eval selected
+                script = self.view.substr(region)
+                output = self.eval_as_ruby(script)
+                self.view.insert(edit, max(region.a, region.b), output)
 
+    def ruby(self):
         try:
-            ruby = self.view.settings().get("ruby_eval").get("ruby")
+            return self.view.settings().get("ruby_eval").get("ruby")
         except AttributeError:
-            ruby = "ruby"
+            return "ruby"
 
-        proc = subprocess.Popen(ruby,
-                 stdout=subprocess.PIPE,
-                 stderr=subprocess.PIPE,
-                 stdin=subprocess.PIPE)
+    def eval_as_ruby(self, script):
+        proc = subprocess.Popen(self.ruby(),
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 stdin=subprocess.PIPE)
 
         output, error = proc.communicate("""
-            result = (lambda {
+            require 'stringio'
+
+            io = StringIO.new
+            begin
+              $stdout = $stderr = io
+              result = (lambda {
                 %s
-            }).call
-            print "#=> " + result.inspect
-        """ % input_str)
+              }).call
+            ensure
+              $stdout = STDOUT
+              $stderr = STDERR
+            end
+
+            if io.string.empty?
+              print result.inspect
+            else
+              print io.string
+            end
+        """ % script)
         output = output.strip()
 
         if proc.poll():
             output += "\n" + error
 
-        if line_eval == False:
-            last_sel = self.view.sel()[-1]
-            insert_pos = max(last_sel.a, last_sel.b)
-        else:
-            last_line = self.view.line(self.view.sel()[-1])
-            insert_pos = last_line.b
-            output = "\n" + output
-
-        self.view.insert(edit, insert_pos, output)
+        return output
